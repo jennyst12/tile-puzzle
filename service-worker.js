c77f57cf4961dfb5b014d7647ae64da49042e066
Jenny S-T <41115621+jennyst12@users.noreply.github.com>
2026-04-11 14:31:46 +0100

    Add improved service worker with icons caching

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'tile-puzzle-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/puzzle.css',
+  '/puzzle.js',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/icon-180.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
